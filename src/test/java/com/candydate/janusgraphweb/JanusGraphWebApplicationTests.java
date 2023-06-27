d419b1761081415258483300480ff3d43c7c0828
yooosungung <dulle2@gmail.com>
2023-06-27 21:56:24 +0900

    add react web

---
+++ src/test/java/com/candydate/janusgraphweb/JanusGraphWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.candydate.janusgraphweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JanusGraphWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
